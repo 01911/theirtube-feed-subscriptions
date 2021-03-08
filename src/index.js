6a9f9d62a0eb15e82b24b5f3da8a5d203246f1f2
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-03-08 10:25:51 -0300

    fix: heroku[router]: at=error code=H10 desc="App crashed" method=GET path="/"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
+var http = require('http');
 const theirtube = require('./theirtube.js');
 const schedule = require('node-schedule');
 const argv = require('yargs').argv;
 const chalk = require('chalk');
 
-(async () => {
+const scrape = async () => {
    await theirtube.initialize();
    //await theirtube.login(process.env.PERSONA1_USERNAME, process.env.PERSONA1_PASSWORD);
    //await theirtube.switchAccount(process.env.PERSONA1_USERNAME);
    await theirtube.scrape([process.env.PERSONA1_USERNAME]);
    return;
-})();
+};
+
+http.createServer(function (req, res) {
+   //res.writeHead(200, { 'Content-Type': 'text/plain' });
+   //res.write('Hello World!');
+   //res.end();
+
+   (async () => {
+      scrape();
+   })();
+}).listen(process.env.PORT || 3333);
